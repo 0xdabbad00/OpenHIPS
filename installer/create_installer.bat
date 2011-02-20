fe831abe7c9b05257b66e0860cc7086bb9fffe1d
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-02-20 21:21:22 +0000

    Fixing up installer

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -1,3 +1,4 @@
-del installer.msi installer.wix*
+del *.msi installer.wix*
 "C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" installer.wxs
 "C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" installer.wixobj
+move installer.msi openhips-setup.msi
