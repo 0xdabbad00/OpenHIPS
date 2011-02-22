41ee37d1de4eeff6438c96f0644e2860de83d460
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-02-22 00:08:22 +0000

    Removed debug comments, started code to add DLL to AppInit for 64-bit processes

--- ohipssvc/OhipsMonitor.cs
+++ ohipssvc/OhipsMonitor.cs
@@ -7,6 +7,10 @@ using System.Diagnostics;
 using System.Reflection;
 using System.IO;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+// TODO MUST Set both 32 and 64 reg key, need to handle 64-bit redirection, as we are only writing to
+//  HKLM\SOFTWARE\wow6432node\...
 
 namespace ohipssvc
 {
@@ -35,50 +39,40 @@ namespace ohipssvc
                 if (WindowsService.running)
                 {
                     EnsureAppInitSet();
-                    Debug.WriteLine("Running in thread");
                 }
-                Thread.Sleep(5 * 1000);
+                // Sleep a minute
+                Thread.Sleep(60 * 1000);
             }
         }
 
         public void EnsureAppInitSet()
         {
             // The purpose of this function is just to set our reg value and ensure other apps
-            // don't accidentally erase our value if they are poorly written and just set appinit instead
+            // don't accidentally erase our value if they just set appinit instead
             // of appending to it
-            Debug.WriteLine("Ensuring app init is set");
-            Microsoft.Win32.RegistryKey key = null;
+            Debug.WriteLine("OHIPS: Ensuring app init is set");
+
+            RegistryKey key = null;
+            RegistryKey localMachineX64View = null;
             try
             {
-                key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(szAppInitKey, true);
-                String value = key.GetValue(szAppInitValue).ToString();
-                Debug.WriteLine(String.Format("Value:{0}", value));
+                // Set 32-bit AppInit_Dll value
+                key = Registry.LocalMachine.OpenSubKey(szAppInitKey, true);
+                EnsureAppInitSet(key, szDllNamePrefix + "32" + ".dll");
+                key.Close();
+                key = null;
 
-                if (value.ToLower().Contains(GetDllPath().ToLower()))
-                {
-                    // Value already contains our DLL, so return
-                    return;
-                }
-
-                // Sanity check so I don't accidentally repeatedly add to this buffer
-                if (value.Length > 1024)
-                {
-                    // TODO Record an error
-                    return;
-                }
-                
-                // Append a space if something is already there
-                if (value.Length != 0)
-                {
-                    value += " ";
-                }
-
-                // Append our DLL path
-                value += GetDllPath();
-                Debug.WriteLine(String.Format("New value:{0}", value));
-
-                // Set registry value
-                key.SetValue(szAppInitValue, value);
+                // Set 64-bit AppInit_Dll value
+                // TODO MUST Uncomment so we'll install on 64-bit builds
+                /*
+                localMachineX64View = RegistryKey.OpenBaseKey(
+                    RegistryHive.LocalMachine, 
+                    RegistryView.Registry64);
+                key = localMachineX64View.OpenSubKey(szAppInitKey, true);
+                EnsureAppInitSet(key, szDllNamePrefix + "64" + ".dll");
+                key.Close();
+                key = null;
+                 */
             }
             finally
             {
@@ -86,16 +80,66 @@ namespace ohipssvc
                 {
                     key.Close();
                 }
+                if (localMachineX64View != null)
+                {
+                    localMachineX64View.Close();
+                }
             }
-
         }
 
-        public static String GetDllPath()
+        public void EnsureAppInitSet(Microsoft.Win32.RegistryKey key, String szDllName)
+        {
+            key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(szAppInitKey, true);
+
+            // Set the relevant values
+            // TODO MAYBE record what these were set to and set them back if we uninstall?
+            String LoadAppInit_DLLs = "LoadAppInit_DLLs";
+            if (key.GetValue(LoadAppInit_DLLs, null) != null)
+            {
+                key.SetValue(LoadAppInit_DLLs, 1);
+            }
+
+            String RequireSignedAppInit_DLLs = "RequireSignedAppInit_DLLs";
+            if (key.GetValue(RequireSignedAppInit_DLLs, null) != null)
+            {
+                key.SetValue(RequireSignedAppInit_DLLs, 1);
+            }
+
+            // Set the AppInit_Dlls value to include our DLL
+            String value = key.GetValue(szAppInitValue).ToString();
+
+            if (value.ToLower().Contains(GetDllPath(szDllName).ToLower()))
+            {
+                // Value already contains our DLL, so return
+                return;
+            }
+
+            // Sanity check so I don't accidentally repeatedly add to this buffer
+            if (value.Length > 1024)
+            {
+                // TODO Record an error
+                return;
+            }
+
+            // Append a space if something is already there
+            if (value.Length != 0)
+            {
+                value += " ";
+            }
+
+            // Append our DLL path
+            value += GetDllPath(szDllName);
+
+            // Set registry value
+            key.SetValue(szAppInitValue, value);
+        }
+
+        public static String GetDllPath(String szDllName)
         {
             // Get the full path to this file
             String filepath = Assembly.GetExecutingAssembly().Location;
             // Get the name of the dll
-            String dllpath = Path.GetDirectoryName(filepath) + "\\" + szDllNamePrefix + "32" + ".dll";
+            String dllpath = Path.GetDirectoryName(filepath) + "\\" + szDllName;
 
             // Get the short path
             StringBuilder shortPath = new StringBuilder(255);
@@ -113,17 +157,16 @@ namespace ohipssvc
             Microsoft.Win32.RegistryKey key = null;
             try
             {
-                String dllPath = GetDllPath();
+                // TODO MUST uninstall from both 32-bit and 64-bit versions
+                // TODO MUST use a regex instead of this junk
+                String dllPath = GetDllPath(szDllNamePrefix + "32" + ".dll");
                 key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(szAppInitKey, true);
                 String value = key.GetValue(szAppInitValue).ToString();
-                Debug.WriteLine(String.Format("Read value:{0}", value));
                 if (value.Length == 0 || !value.ToLower().Contains(dllPath.ToLower()))
                 {
                     // Dll path isn't there
                     return;
                 }
-
-                Debug.WriteLine(String.Format("Finding start"));
 
                 int dllPathStart = value.ToLower().IndexOf(dllPath.ToLower());
                 int dllPathEnd = dllPathStart + dllPath.Length+1;
@@ -131,8 +174,7 @@ namespace ohipssvc
                 {
                     dllPathEnd = value.Length;
                 }
-                Debug.WriteLine(String.Format("Indexes set to {0} and {1}", dllPathStart, dllPathEnd));
-
+                
                 // Remove separator
                 if (dllPathStart - 1 >= 0 && isSeparator(value[dllPathStart-1]))
                 {
@@ -143,12 +185,10 @@ namespace ohipssvc
                 {
                     dllPathEnd++;
                 }
-                Debug.WriteLine(String.Format("Corrected indexes set to {0} and {1}", dllPathStart, dllPathEnd));
-            
+                
                 String prefix = value.Substring(0, dllPathStart);
                 String suffix = value.Substring(dllPathEnd);
 
-                Debug.WriteLine(String.Format("Have strings \"{0}\" and \"{1}\"", prefix, suffix));
                 if (prefix.Length != 0 && suffix.Length != 0)
                 {
                     // Add separator
@@ -156,10 +196,9 @@ namespace ohipssvc
                 }
 
                 String newValue = prefix + suffix;
-                Debug.WriteLine(String.Format("New value:{0}", newValue));
+                
                 // Set registry value
                 key.SetValue(szAppInitValue, newValue);
-
             }
             finally
             {
