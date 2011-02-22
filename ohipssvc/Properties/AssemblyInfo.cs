2eef949d5b45238b4c66f61da50cfd1938704a08
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-02-22 01:31:13 +0000

    Changed version to 0.0.0.1 and added resources and manifests so all files have version tabs

---
+++ ohipssvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenHIPS")]
+[assembly: AssemblyDescription("Protects against exploitation")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("0xdabbad00")]
+[assembly: AssemblyProduct("OpenHIPS")]
+[assembly: AssemblyCopyright("GNU Public License v3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("868a653f-e232-447a-89ad-a0abe2580513")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
