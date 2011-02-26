ddfe869ed8865e2bcc2fa7d2683172ee91700f6e
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-02-26 20:34:41 +0000

    No longer copying debug files to Release dir, but am using debug files in installer for 32-bit DLLs

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -1,5 +1,4 @@
 del *.msi installer.wix*
-copy ..\bin\Debug\*.dll ..\bin\Release\.
 "C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" installer.wxs
 "C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" installer.wixobj
 move installer.msi openhips-setup.msi
