401a3e77666c1922775ad43e736dd969cc493fb4
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-03-12 21:37:30 +0000

    Basic settings layout (ugly)

--- ohipsui/TrayIcon.cs
+++ ohipsui/TrayIcon.cs
@@ -12,7 +12,17 @@ namespace ohipsui
         private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.NotifyIcon TrayNotifyIcon;
         private System.Windows.Forms.ContextMenuStrip TrayContextMenuStrip;
-        private Button CloseBtn;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.Label LblProcessSelection;
+        private System.Windows.Forms.ListBox ListBoxProcessSelector;
+        private System.Windows.Forms.Label LblMemMax;
+        private System.Windows.Forms.TextBox TextBoxMemMax;
+        private System.Windows.Forms.Label LblNopSledMin;
+        private System.Windows.Forms.TextBox TextBoxNopSledMin;
+        private System.Windows.Forms.Label LblNullPrealloc;
+        private System.Windows.Forms.CheckBox ChkBoxNullPrealloc;
+        private System.Windows.Forms.Label LblGenericPrealloc;
+        private System.Windows.Forms.CheckBox ChkBoxGenericPrealloc;
         private System.Windows.Forms.ToolStripMenuItem SettingsToolStripMenuItem;
 
 
@@ -123,22 +133,85 @@ namespace ohipsui
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrayIcon));
-            this.CloseBtn = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
 
             int formWidth = 500;
             int formHeight = 300;
+            int margin = 15;
 
             // 
             // CloseBtn
+            //
+            this.BtnClose.Text = "Close";
+            this.BtnClose.Location = new System.Drawing.Point(formWidth - this.BtnClose.Size.Width - margin, formHeight - this.BtnClose.Size.Height * 2 - margin);
+            this.BtnClose.TabIndex = 0;
+            this.BtnClose.Click += new System.EventHandler(this.CloseBtn_Click);
+
+            //
+            // Process selector
+            //
+            this.LblProcessSelection = new System.Windows.Forms.Label();
+            this.LblProcessSelection.Text = "Process name";
+            this.LblProcessSelection.Location = new System.Drawing.Point(margin, margin);
+
+            
+            this.ListBoxProcessSelector = new System.Windows.Forms.ListBox();
+            this.ListBoxProcessSelector.Text = "Process names";
+            this.ListBoxProcessSelector.Location = new System.Drawing.Point(margin, this.LblProcessSelection.Location.Y + this.LblProcessSelection.Size.Height);
+            this.ListBoxProcessSelector.ScrollAlwaysVisible = true;
+            ListBoxProcessSelector.BeginUpdate();
+            // Loop through and add 50 items to the ListBox.
+            for (int x = 1; x <= 5; x++)
+            {
+                ListBoxProcessSelector.Items.Add("Item " + x.ToString());
+            }
+            // Allow the ListBox to repaint and display the new items.
+            ListBoxProcessSelector.EndUpdate();
+            //ListBoxProcessSelector.SetSelected(1, true);
+
+            //
+            // Mem max
+            //
+            int col2 = this.ListBoxProcessSelector.Location.X + this.ListBoxProcessSelector.Width + margin;
+            this.LblMemMax = new System.Windows.Forms.Label();
+            this.LblMemMax.Text = "Memory max";
+            this.LblMemMax.Location = new System.Drawing.Point(col2, margin);
+
+            this.TextBoxMemMax = new System.Windows.Forms.TextBox();
+            this.TextBoxMemMax.Text = "100";
+            this.TextBoxMemMax.Location = new System.Drawing.Point(col2, LblMemMax.Location.Y + LblMemMax.Height);
+
             // 
-            this.CloseBtn.Size = new System.Drawing.Size(75, 23);
-            this.CloseBtn.Location = new System.Drawing.Point(formWidth - this.CloseBtn.Size.Width - 15, formHeight - this.CloseBtn.Size.Height - 15);
-            this.CloseBtn.Name = "Close";
-            this.CloseBtn.TabIndex = 0;
-            this.CloseBtn.Text = "Close";
-            this.CloseBtn.UseVisualStyleBackColor = true;
-            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // Nop sled min
+            //
+            this.LblNopSledMin = new System.Windows.Forms.Label();
+            this.LblNopSledMin.Text = "Nop sled minimum length";
+            this.LblNopSledMin.Location = new System.Drawing.Point(col2, TextBoxMemMax.Location.Y + TextBoxMemMax.Height+margin);
+
+            this.TextBoxNopSledMin = new System.Windows.Forms.TextBox();
+            this.TextBoxNopSledMin.Text = "100";
+            this.TextBoxNopSledMin.Location = new System.Drawing.Point(col2, LblNopSledMin.Location.Y + LblNopSledMin.Height);
+
+            // 
+            // Null pre-alloc
+            //
+            this.ChkBoxNullPrealloc = new System.Windows.Forms.CheckBox();
+            this.ChkBoxNullPrealloc.Location = new System.Drawing.Point(col2, TextBoxNopSledMin.Location.Y + TextBoxNopSledMin.Height);
+
+            this.LblNullPrealloc = new System.Windows.Forms.Label();
+            this.LblNullPrealloc.Text = "Pre-alloc null address";
+            this.LblNullPrealloc.Location = new System.Drawing.Point(ChkBoxNullPrealloc.Location.X + ChkBoxNullPrealloc.Width, ChkBoxNullPrealloc.Location.Y);
+
+            // 
+            // Generic pre-alloc
+            //
+            this.ChkBoxGenericPrealloc = new System.Windows.Forms.CheckBox();
+            this.ChkBoxGenericPrealloc.Location = new System.Drawing.Point(col2, ChkBoxNullPrealloc.Location.Y + ChkBoxNullPrealloc.Height);
+
+            this.LblGenericPrealloc = new System.Windows.Forms.Label();
+            this.LblGenericPrealloc.Text = "Pre-alloc generic addresses";
+            this.LblGenericPrealloc.Location = new System.Drawing.Point(ChkBoxGenericPrealloc.Location.X + ChkBoxGenericPrealloc.Width, ChkBoxGenericPrealloc.Location.Y);
 
 
             // 
@@ -147,8 +220,22 @@ namespace ohipsui
             this.ClientSize = new System.Drawing.Size(formWidth, formHeight);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("icon.ico")));
             this.ControlBox = false;
-            this.Controls.Add(this.CloseBtn);
-            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.LblProcessSelection);
+            this.Controls.Add(this.ListBoxProcessSelector);
+            this.Controls.Add(this.LblMemMax);
+            this.Controls.Add(this.TextBoxMemMax);
+            this.Controls.Add(this.LblNopSledMin);
+            this.Controls.Add(this.TextBoxNopSledMin);
+
+
+            this.Controls.Add(this.ChkBoxNullPrealloc);
+            this.Controls.Add(this.LblNullPrealloc);
+            this.Controls.Add(this.ChkBoxGenericPrealloc);
+            this.Controls.Add(this.LblGenericPrealloc);
+
+            this.MinimumSize = new System.Drawing.Size(formWidth, formHeight);
+            this.MaximumSize = new System.Drawing.Size(formWidth, formHeight);
             this.Name = "TrayIcon";
             this.Text = "OpenHIPS Settings";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
@@ -158,7 +245,8 @@ namespace ohipsui
 
         private void CloseBtn_Click(object sender, EventArgs e)
         {
-            HideSettingsForm();
+            Application.Exit(); // TODO REMOVE
+            // TODO UNCOMMENT HideSettingsForm();
         }
     }
 }
