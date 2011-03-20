2dc4bfd033caa93144cd41b5a9629fff8f9a7174
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-03-20 14:53:40 +0000

    I couldn't get the service to easily start up the UI for all users, so now the UI is launched by the installer on install, and then by the Run key for all subsequent logins.  This is not a perfect solution, and will change later, but good enough for now.

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -1,4 +1,4 @@
 del *.msi installer.wix*
-"C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" installer.wxs
-"C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" installer.wixobj
+"C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" -ext WixUtilExtension installer.wxs 
+"C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUtilExtension installer.wixobj
 move installer.msi openhips-setup.msi
