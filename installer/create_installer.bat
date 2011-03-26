a7dabc3010bb05ecb5abe7f5554936a18d72974a
0xdabbad00 <0xdabbad00@aded653e-3d15-11e0-8395-1d422fa2545a>
2011-03-26 22:44:33 +0000

    Installer no longer writes to the registry, and now outputs an installer containing the debug files.

--- installer/create_installer.bat
+++ installer/create_installer.bat
@@ -2,3 +2,7 @@ del *.msi installer.wix*
 "C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" -ext WixUtilExtension installer.wxs 
 "C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUtilExtension installer.wixobj
 move installer.msi openhips-setup.msi
+del installer.wix*
+"C:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" -dIsDebug="true" -ext WixUtilExtension installer.wxs 
+"C:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -ext WixUtilExtension installer.wixobj
+move installer.msi openhips-setup_debug.msi
